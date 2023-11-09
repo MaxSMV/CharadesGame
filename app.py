f04a9853f2e5184659485f1ef1e9a8988e05c501
MaxSMV <maxstefankiv2001@gmail.com>
2023-11-09 12:55:04 +0200

    ver_1.1

--- app.py
+++ app.py
@@ -1,76 +1,67 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from models import db, Game
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from moduls import db, User
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
+app.config['SECRET_KEY'] = 'your-secret-key'
+
 db.init_app(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        role = request.form['role']
-        game = Game.query.first()
-        if not game:
-            game = Game()
-            db.session.add(game)
-        if role == 'Actor' and not game.actor_username:
-            game.actor_username = username
-        elif role == 'Guesser' and not game.guesser_username:
-            game.guesser_username = username
-        else:
-            flash('Role is already taken', 'error')
-            return render_template('login.html')
-        db.session.commit()
-        return redirect(url_for('game', username=username, role=role))
-    return render_template('login.html')
-
-@app.route('/game/<username>/<role>')
-def game(username, role):
-    game = Game.query.first()
-    if not game:
-        flash('No active game', 'error')
-        return redirect(url_for('login'))
-    if role == 'Actor' and game.actor_username == username:
-        return render_template('actor.html', username=username, game=game)
-    elif role == 'Guesser' and game.guesser_username == username:
-        return render_template('guesser.html', username=username, game=game)
-    else:
-        flash('Invalid role or username', 'error')
-        return redirect(url_for('login'))
-
-@app.route('/start_game', methods=['POST'])
-def start_game():
-    game = Game.query.first()
-    if game and game.actor_username and game.guesser_username:
-        game.is_active = True
-        db.session.commit()
-        return redirect(url_for('game', username=game.actor_username, role='Actor'))
-    flash('Game cannot be started', 'error')
+@app.route("/")
+def index():
     return redirect(url_for('login'))
 
-@app.route('/select_word', methods=['POST'])
-def select_word():
-    word = request.form['word']
-    game = Game.query.first()
-    if game:
-        game.word_to_guess = word
+
+@app.route("/game")
+def game():
+    if 'user_id' not in session:
+        flash('You must be logged in to view the game.')
+        return redirect(url_for('login'))
+    return render_template("game.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is not None:
+            flash('Username already exists.')
+            return redirect(url_for('register'))
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('game', username=game.actor_username, role='Actor'))
-    return "No active game", 404
+        session['user_id'] = new_user.id
+        flash('Successfully registered and logged in!')
+        return redirect(url_for('game'))
+    return render_template("register.html")
 
-@app.route('/guess', methods=['POST'])
-def guess():
-    guess = request.form['guess']
-    game = Game.query.first()
-    if game and game.word_to_guess:
-        # TODO: Check the guess against the word_to_guess and update game state
-        pass  # Placeholder
-    return redirect(url_for('game', username=game.guesser_username, role='Guesser'))
 
-if __name__ == '__main__':
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            flash('Logged in successfully!')
+            return redirect(url_for('game'))
+        flash('Invalid username or password.')
+    return render_template("login.html")
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.clear()
+    flash('You have been logged out.')
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
